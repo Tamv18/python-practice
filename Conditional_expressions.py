1840d66b53eca8618e13fded59225fb045fa0de8
Tamanna Wadhwa <sadpikachu158@gmail.com>
2025-08-12 15:01:43 +0530

    Create Conditional_expressions.py
    I used conditional expression to check if the condition is true. If it is, it'll print the greatest number. And also, I chose input from the user as an integer that stores our value in a1, a2, a3 and a4.

---
+++ Conditional_expressions.py
@@ -0,0 +1,18 @@
+a1 = int(input("Enter number 1: "))
+a2 = int(input("Enter number 2: "))
+a3 = int(input("Enter number 3: "))
+a4 = int(input("Enter number 4: "))
+
+if(a1>a2 and a1>a3 and a1>4):
+    print(" The greatest number is a1", a1)
+if(a2>a1 and a2>a3 and a2>4):
+    print(" The greatest number is a2", a2)
+if(a3>a2 and a3>a1 and a3>4):
+    print(" The greatest number is a3", a3)
+if(a4>a2 and a4>a3 and a4>1):
+    print(" The greatest number is a4", a4)
+
+'''
+This tells us what is the greatest number 
+  out of 4 numbers chosen from the user
+'''
